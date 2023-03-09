2a4d04a7227b85b8d4fca952ef47c65fe24a8cf6
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-03-09 13:22:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
 
+
+
+document.getElementById('addTodoBtn').addEventListener('click',
+			()=> {
+console.log("welcome");
+let input = document.getElementById("newTodoInput");
+const node = document.createElement("li");
+document.getElementById('todoList').appendChild(node).innerText=input.value;
+				input.value = "";
+				
+});
